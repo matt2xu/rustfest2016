aa5014b8c4d56bb834706f8136ffc314e82bcde0
Matthieu Wipliez <matthieu.wipliez@harmonicinc.com>
2016-09-12 15:30:30 +0200

    add a couple of printlns and simplify code returning the future

--- src/bin/tokio_ident_service.rs
+++ src/bin/tokio_ident_service.rs
@@ -7,7 +7,6 @@ extern crate rustfest;
 
 use std::io;
 
-use futures::Future;
 use futures::stream::Empty;
 
 use tokio_core::reactor::Core;
@@ -26,18 +25,19 @@ fn main() {
 
     server::listen(&handle, addr, move |stream: TcpStream| {
         let ip = stream.peer_addr().unwrap().ip();
+        println!("incoming connection from {}", ip);
+
         let service = simple_service(move |request: String| {
             let query: ident::Query = request.parse().unwrap();
             println!("got query \"{}\" from {}", query, ip);
             let reply = query.process(&ip);
 
             // Return the response as an immediate future
-            futures::finished(reply.to_string()).map(|body| {
-                pipeline::Message::WithoutBody(body) as pipeline::Message<String, Empty<(), io::Error>>
-            })
+            futures::finished(pipeline::Message::WithoutBody(reply.to_string()) as pipeline::Message<String, Empty<(), io::Error>>)
         });
         pipeline::Server::new(service, transport::new_line_transport(stream))
     }).unwrap();
 
+    println!("Listening on {}", addr);
     core.run(futures::empty::<(), ()>()).unwrap();
 }
